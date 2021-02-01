21fe1930ffd8a0292ddfa24eaa3ae7d919d598ca
Vamsi Manubolu <71382042+VamsiManubolu@users.noreply.github.com>
2021-02-01 16:11:08 +0530

    added comments to my function.php file

--- Function.php
+++ Function.php
@@ -1,9 +1,16 @@
+/* the following code is pasted in C:\xampp\htdocs\wptest\wp-includes\functions.php to work */
+
 <?php
+//if user want to change his profile we are filtering  get_avatar function
 add_filter( 'get_avatar' , 'my_custom_avatar' , 1 , 5 );
+// in the above funtion 1 refer to priority for the event to occur
+//5 refer to number of arguments that function(my_custom_avatar) accepts
 
 function my_custom_avatar( $avatar, $id_or_email, $size, $default, $alt ) {
-	
+    
     $user = false;
+
+    // Get user by user ID or Mail id
 
     if ( is_numeric( $id_or_email ) ) {
 
@@ -23,23 +30,25 @@ function my_custom_avatar( $avatar, $id_or_email, $size, $default, $alt ) {
 
     if ( $user && is_object( $user ) ) {
 
-    	$current_user=wp_get_current_user();
+        // get the array of user ids existing in usermeta table
 
-    	$id=$user->ID;
-
-
+        $id=$user->ID;
 
 
-        // if ( $user->data->ID==$id ) {
-        	
-        	
-            $avatar =get_user_meta( $id, 'userurl',true );
 
-            $avatar = "<img alt='{$alt}' src='{$avatar}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
-            // }
+
+        
+        //  get the meta value for the userid from usermeta table   
+        $avatar =get_user_meta( $id, 'userurl',true );
+
+        // get the img markup
+
+        $avatar = "<img alt='{$alt}' src='{$avatar}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
+        
 
     }
-
+    // return our new user avatar
     return $avatar;
 }
+
 ?>
